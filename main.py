96eb78eb6ddc29bbc99e639819a9c440eaf293ac
1 <1>
2020-08-04 19:03:49 +0300

    update

--- main.py
+++ main.py
@@ -19,6 +19,9 @@ def webhook():
 
 
 if __name__ == '__main__':
+    import time
+    bot.remove_webhook()
+    time.sleep(1)
     bot.set_webhook(
         url=config.WEBHOOK_URL,
         certificate=open('webhook_cert.pem', 'r')
