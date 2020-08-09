5f859fdeb2c487ffeed5c64b40f50e1eebb08566
Unakepro <68908289+Unakepro@users.noreply.github.com>
2020-08-09 15:34:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from webshop.bot.main import bot, start_bot
+from webshop.bot.main import bot
 from webshop.bot import config
 from flask import Flask, request, abort
 from flask_restful import Api
@@ -6,6 +6,17 @@ from telebot.types import Update
 
 
 app = Flask(__name__)
+
+@app.route(config.WEBHOOK_PATH, methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = Update.de_json(json_string)
+        bot.process_new_updates([update])
+        return ''
+    else:
+        abort(403)
+
 
 if __name__ == '__main__':
     import time
@@ -21,15 +32,3 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-@app.route(config.WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
-
-if __name__ == "__main__":
-    start_bot()
