282d7e7f66121bb044cebb4b758e55d0d41ebaa1
1 <1>
2020-08-09 19:19:12 +0300

    congo

--- main.py
+++ main.py
@@ -18,32 +18,12 @@ def webhook():
 
 
 if __name__ == '__main__':
+    import time
+
+    bot.remove_webhook()
     bot.set_webhook(
         config.WEBHOOK_URL,
         certificate=open('webhook_cert.pem', 'r')
     )
 
 app = Flask(__name__)
-
-
-@app.route(config.WEBHOOK_PATH, methods=['POST'])
-def webhook():
-    if request.headers.get('content-type') == 'application/json':
-        json_string = request.get_data().decode('utf-8')
-        update = Update.de_json(json_string)
-        bot.process_new_updates([update])
-        return ''
-    else:
-        abort(403)
-
-
-if __name__ == '__main__':
-    import time
-
-    bot.remove_webhook()
-    time.sleep(1)
-    bot.set_webhook(
-        url=config.WEBHOOK_URL,
-        certificate=open('webhook_cert.pem', 'r')
-    )
-    app.run(debug=True)
