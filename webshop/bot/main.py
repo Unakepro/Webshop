b2077eb81a3866fc589125bf57cd6e969e03fd96
1 <1>
2020-08-11 13:16:51 +0300

    not

--- webshop/bot/main.py
+++ webshop/bot/main.py
@@ -175,11 +175,11 @@ def add_my_cart(call):
 
     if not MyCart.objects(user_id=call.from_user.id, product_id=product_id):
         MyCart.objects.create(user_id=call.from_user.id, product_id=product_id)
-        bot.answer_callback_query(callback_query_id=call.message.chat.id, text="Товар успешно добавлен в корзину",
+        bot.answer_callback_query(callback_query_id=call.id, text="Товар успешно добавлен в корзину",
                                   show_alert=False)
     else:
         MyCart.objects(user_id=call.from_user.id, product_id=product_id).update(inc__value=1)
-        bot.answer_callback_query(callback_query_id=call.message.chat.id, text="Товар успешно добавлен в корзину",
+        bot.answer_callback_query(callback_query_id=call.id, text="Товар успешно добавлен в корзину",
                                   show_alert=False)
 
 
