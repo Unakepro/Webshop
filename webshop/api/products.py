b5a031da5e1d60b81eabf711aec172edcf52e56d
1 <1>
2020-08-11 14:14:32 +0300

    lol

--- webshop/api/products.py
+++ webshop/api/products.py
@@ -58,7 +58,7 @@ def post():
 
             category = res['category']
             del res['category']
-            with open("../db/images/routers/asus.jpg") as image:
+            with open("webshop/db/images/routers/asus.jpg") as image:
                 Products.objects.create(**res, category=ObjectId(category), image=image)
         except ValidationError as err:
             res = err.messages
